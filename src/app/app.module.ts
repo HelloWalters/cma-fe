ea887571e9b9cc331188ba867f821ecbac317a0f
Andrew Walters <andrew@hellowalters.com>
2023-01-04 19:42:27 +0000

    update to material 15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
 import { NewCampaignFormComponent } from './shared/components/forms/new-campaign-form/new-campaign-form.component';
 import { CampaignDisplayListComponent } from './shared/modules/campaigns/campaign-list/campaign-display-list/campaign-display-list.component';
 import { DefaultListComponent } from './shared/components/lists/default-list/default-list.component';
 import { CurrentUserComponent } from './shared/components/users/current-user/current-user.component';
-import { MatListModule } from '@angular/material/list';
+import { MatLegacyListModule as MatListModule } from '@angular/material/legacy-list';
 import { HttpClientModule } from '@angular/common/http';
-import {MatInputModule} from '@angular/material/input';
+import {MatLegacyInputModule as MatInputModule} from '@angular/material/legacy-input';
 
 @NgModule({
   declarations: [
